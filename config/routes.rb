0d595c6b3c18eb124875f82631a6ea687bd7899d
Corey Martin <coreym@gmail.com>
2015-12-26 17:34:42 -0700

    simple document list / show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
+
+  root to: "dashboard#index"
+
+  resources :documents
 end
