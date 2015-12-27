08df58b837c7f23527a690fa00eda703fbaeeed4
Corey Martin <coreym@gmail.com>
2015-12-27 15:57:24 -0700

    basic doc editing

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,4 +2,20 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
   end
+
+  def edit
+    @document = Document.find(params[:id])
+  end
+
+  def update
+    @document = Document.find(params[:id])
+    @document.update!(document_params)
+    redirect_to document_path(@document)
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:title, :body)
+  end
 end
